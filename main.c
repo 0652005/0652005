0a446eeced8e1bf25d927edef65d55d95e392673
0652005 <a0989921857@gmail.com>
2018-05-18 11:04:24 +0800

    third commit

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <time.h> 
 void ran(void);
 void bubble(void);
-int G[10];
+int GG[10];
 int input=0,output=0;
 int i,c,j,k;
 int main (void){
@@ -12,7 +12,7 @@ int main (void){
 	bubble();
 	scanf("%d",&input);
 for(i=0;i<=9;i++){
-	if(G[i]==input){
+	if(GG[i]==input){
 	printf("²Ä%d­Ó",i+1);
 		output=1;
 	}
@@ -26,17 +26,17 @@ void bubble(void){
 int temp;
 	for(c = 0; c <=9; c++){
 		for(k= c; k < 10; k++){
-			if( G[k] < G[c] ) {
-				temp = G[k];
-				G[k] = G[c];
-				G[c] = temp;	
+			if( GG[k] < GG[c] ) {
+				temp = GG[k];
+				GG[k] = GG[c];
+				GG[c] = temp;	
 		}
 	}
 }
 for(j =0;j<=8;j++){
-	printf("%d,",G[j]);
+	printf("%d,",GG[j]);
 	}  
-	printf("%d",G[9]);
+	printf("%d",GG[9]);
 }
 
 void ran (void){
@@ -44,24 +44,24 @@ void ran (void){
 	srand(time(NULL));
 
 	for(i=0;i<=10;i++){
-		G[i]=(rand()%91)+10;
+		GG[i]=(rand()%91)+10;
         }
         for(i=0;i<=10;i++){
 		for(j=i+1;j<=10;j++){
-			if(G[i]==G[j]){
-				G[i]=(rand()%91)+10;
-							if(G[i]==G[j]){
+			if(GG[i]==GG[j]){
+				GG[i]=(rand()%91)+10;
+							if(GG[i]==GG[j]){
 				printf("");
-				G[i]=(rand()%91)+10;
-				if(G[i]==G[j]){
-				G[i]=(rand()%91)+10;
+				GG[i]=(rand()%91)+10;
+				if(GG[i]==GG[j]){
+				GG[i]=(rand()%91)+10;
 			}	
 		}							
     }
 }
 }
 for(j =0;j<=8;j++){
-		printf("%d,",G[j]);
+		printf("%d,",GG[j]);
 	}
-	printf("%d",G[9]);
+	printf("%d",GG[9]);
 }
